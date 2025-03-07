454ff3fbff22cde706a020761d20c8c6d27f717b
Samet Gurdal <samet.devhub@gmail.com>
2025-03-07 04:43:31 +0300

    Home page updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,48 @@
 import React from "react";
 import { Container, Typography, Box } from "@mui/material";
 import "./Home.css";
+import library from "../../assets/library.jpg";
+import Button from '@mui/material/Button';
 
 function Home() {
   return (
-    <Container>
-      <Box className="home">
-        <Typography variant="h4" gutterBottom>
-          Welcome to the Library Management System
-        </Typography>
-        <Typography variant="h6">
-          You can do all book, author, and category transactions here.
-        </Typography>
-      </Box>
-    </Container>
-  )
+    <Box
+      sx={{
+        backgroundImage: `url(${library})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        height: "calc(100vh - 68.5px)", // Navbar yüksekliği çıkarıldı (64px varsayılan)
+        width: "100vw",
+        overflow: "hidden",
+        position: "relative",
+        "&::before": {
+          content: '""',
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          zIndex: 1,
+        },
+      }}
+    >
+      <Container sx={{ position: "relative", zIndex: 2 }}>
+        <Box className="home">
+          <Typography variant="h4" gutterBottom sx={{ color: "white" }}>
+            Welcome to the Library Management System
+          </Typography>
+          <Typography variant="h6" sx={{ color: "white" }}>
+            You can do all book, author, and category transactions here.
+          </Typography>
+          <Button variant="outlined" href="/book"
+          sx={{ color: "white", borderColor: "white", marginTop: 2 }}
+          >Get Started</Button>
+        </Box>
+      </Container>
+    </Box>
+  );
 }
 
-export default Home
+export default Home;
